06e379096bb174f70b53c3581b57b9aa98cb02e7
julio <julio.cesar.roseli@gmail.com>
2023-08-30 23:46:43 -0300

    crição dos metodos na interface PessoaDao

--- src/model/dao/PessoaDao.java
+++ src/model/dao/PessoaDao.java
@@ -9,7 +9,11 @@ public interface PessoaDao {
 	void insert(Pessoa obj);
 	void update(Pessoa obj);
 	void deleteById(Pessoa obj);
+	void EntradaId(Pessoa obj, Integer id);
+	void SaidaId(Pessoa obj, Integer id);
 	Pessoa FindById(Integer id);
 	List<Pessoa> findAll();
 	List<Pessoa> findBYbloco();
+	
+	
 }
